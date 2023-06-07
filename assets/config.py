d7b30ff861d44a97dbdc5cc40ac4940e827c3e57
ExalID <131982900+ExalID@users.noreply.github.com>
2023-06-08 01:01:42 +0700

    Delete config.py

--- assets/config.py
+++
@@ -1,8 +0,0 @@
-# JANGAN DIRUBAH NTAR ERROR TAU RASA LU BEGO
-USN = "Mozilla/5.0 (Linux; Android 12; SM-G981U1 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/105.0.5195.136 Mobile Safari/537.36 Instagram 256.0.0.18.105 Android (31/12; 450dpi; 1080x2322; samsung; SM-G981U1; x1q; qcom; in_ID; 407843045)"
-SYNC = "https://i.instagram.com/api/v1/qe/sync/"
-API_USER = "https://i.instagram.com/api/v1/users/web_profile_info/?username=%s"
-API_GRAPH = "https://i.instagram.com/api/v1/friendships/"
-API_FOLLOWERS = "https://i.instagram.com/api/v1/friendships/%s/followers/?count=100"
-API_FOLLOWING = "https://i.instagram.com/api/v1/friendships/%s/following/?count=100"
-API_LOGIN = "https://i.instagram.com/api/v1/accounts/login/"
