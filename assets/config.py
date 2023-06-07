bc615213d05683261354c1f638ad690a3b8a48e2
ExalID <131982900+ExalID@users.noreply.github.com>
2023-06-08 01:10:21 +0700

    Update config.py

--- assets/config.py
+++ assets/config.py
@@ -1,3 +1,4 @@
+# JANGAN DIUBAH NTAR ERROR NANGIS LU GOBLOK!!
 USN = "Mozilla/5.0 (Linux; Android 10; SM-A920F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.71 Mobile Safari/537.36 Instagram 243.0.0.16.111 Android (29/10; 420dpi; 1080x2094; samsung; SM-A920F; a9y18qlte; qcom; in_ID; 382290498)"
 API_USER = "https://i.instagram.com/api/v1/users/web_profile_info/?username=%s"
 API_GRAPH = "https://i.instagram.com/api/v1/friendships/"
