4cf99a7c2a8af0241bf2f90b9eb36a4e818689dd
ExalID <131982900+ExalID@users.noreply.github.com>
2023-06-23 15:47:56 +0700

    Add files via upload

--- assets/config.py
+++ assets/config.py
@@ -1,5 +1,5 @@
-# JANGAN DIUBAH NTAR ERROR NANGIS LU GOBLOK!!
 USN = "Mozilla/5.0 (Linux; Android 10; SM-A920F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.71 Mobile Safari/537.36 Instagram 243.0.0.16.111 Android (29/10; 420dpi; 1080x2094; samsung; SM-A920F; a9y18qlte; qcom; in_ID; 382290498)"
+USN2 = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 API_USER = "https://i.instagram.com/api/v1/users/web_profile_info/?username=%s"
 API_GRAPH = "https://i.instagram.com/api/v1/friendships/"
 API_FOLLOWERS = "/followers/?count=100"
